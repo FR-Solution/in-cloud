b5645d67a02c6bcb83ce6d19e933b43f48b761a0
Dmitrii Putilin <dlputilin@dobry-kot.ru>
2024-10-30 20:06:06 +0300

    init

---
+++ documentation/sidebars.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-default-export */
+// import { toNft } from './src/sidebars/technical-docs/k8sHW'
+
+
+const sidebars = {
+
+  techDocs: [
+    {
+      type: 'doc',
+      label: 'Kubernetes-The-Hard-Way',
+      id: 'tech-docs/k8s-hello',
+    },
+    {
+      type: 'doc',
+      label: 'ClusterAPI',
+      id: 'tech-docs/clusterAPI',
+    },
+    {
+      type: 'doc',
+      label: 'Bcloud Manifests',
+      id: 'tech-docs/manifests',
+    },
+
+  ],
+}
+
+export default sidebars
