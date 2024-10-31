c27d53abc05b1ae605ae326a4a5d8cd20aeb02dc
Dmitrii Putilin <dlputilin@dobry-kot.ru>
2024-10-31 16:01:25 +0300

    форматирование

--- documentation/sidebars.ts
+++ documentation/sidebars.ts
@@ -1,27 +1,16 @@
 /* eslint-disable import/no-default-export */
-// import { toNft } from './src/sidebars/technical-docs/k8sHW'
+import { kubernetesTHW } from './src/sidebars/technical-docs/kubernetes-thw'
 
 
 const sidebars = {
 
   techDocs: [
     {
-      type: 'doc',
+      type: 'category',
       label: 'Kubernetes-The-Hard-Way',
-      id: 'tech-docs/k8s-hello',
-    },
-    {
-      type: 'doc',
-      label: 'ClusterAPI',
-      id: 'tech-docs/clusterAPI',
-    },
-    {
-      type: 'doc',
-      label: 'Bcloud Manifests',
-      id: 'tech-docs/manifests',
-    },
-
-  ],
+      items: kubernetesTHW
+    }
+  ]
 }
 
 export default sidebars
---
+++ documentation/src/sidebars/technical-docs/kubernetes-thw.ts
@@ -0,0 +1,52 @@
+export const kubernetesTHW = [
+  {
+    type: 'doc',
+    label: 'Введение',
+    id: 'tech-docs/kubernetes-thw/introduction',
+  },
+  {
+    type: 'doc',
+    label: 'Компоненты',
+    id: 'tech-docs/kubernetes-thw/components',
+  },
+  {
+    type: 'doc',
+    label: 'Архитектура',
+    id: 'tech-docs/kubernetes-thw/arch',
+  },
+  {
+    type: 'doc',
+    label: 'Пререквизиты',
+    id: 'tech-docs/kubernetes-thw/step-by-step-os',
+  },
+  {
+    type: 'doc',
+    label: 'Фазы Kubeadm',
+    id: 'tech-docs/kubernetes-thw/phase-kubeadm',
+  },
+  {
+    type: 'doc',
+    label: 'Настройка Сертификаты',
+    id: 'tech-docs/kubernetes-thw/step-by-step-certs',
+  },
+  {
+    type: 'doc',
+    label: 'Настройка Static Pods',
+    id: 'tech-docs/kubernetes-thw/step-by-step-static-pods',
+  },
+  {
+    type: 'doc',
+    label: 'Запуск кластера',
+    id: 'tech-docs/kubernetes-thw/k8s-start',
+  },
+  {
+    type: 'doc',
+    label: 'Модификация под Kubeadm',
+    id: 'tech-docs/kubernetes-thw/step-by-step-kubeadm-aditional',
+  },
+  {
+    type: 'doc',
+    label: 'Trubleshooting',
+    id: 'tech-docs/kubernetes-thw/k8s-trubleshooting',
+  },
+]
